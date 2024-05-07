01c06c5eca3ca7a735f4d33e1ae871ee5b1eaa33
saerom <sagesaeromkim@gmail.com>
2024-05-07 10:31:15 +0900

    chore: sourcemap 관련 설정 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
     <>
-      <div>three.js</div>
+      <Canvas>
+        <OrbitControls />
+        <mesh>
+          <ambientLight intensity={1} />
+          <directionalLight position={[-1, 0, 1]} intensity={0.5} />
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial attach="material" color={0xa3b18a} />
+        </mesh>
+      </Canvas>
     </>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
